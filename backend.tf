3d498e2269950d6c0a105c328785c9f23547015b
gad <nday.gady@gmail.com>
2025-11-29 16:40:35 -0700

    feat(uat): deploying ecs infrastructure [sos-05]

---
+++ backend.tf
@@ -0,0 +1,10 @@
+
+terraform {
+  backend "s3" {
+    bucket       = "remote-gn-terrraform" # Replace with your bucket name
+    key          = "uat/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = false
+  }
+}
